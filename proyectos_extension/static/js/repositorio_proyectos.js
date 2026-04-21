6cdddd7f4f91cf86af2ea6d0ebd530deea3417ce
yfmartinez-fcyt <yfmartinez@fctunca.edu.py>
2026-04-21 00:23:52 -0300

    Arregla repositorio_proyectos y detalle_repositorio

---
+++ proyectos_extension/static/js/repositorio_proyectos.js
@@ -0,0 +1,8 @@
+document.addEventListener("DOMContentLoaded", () => {
+    console.log("Repositorio de proyectos cargado");
+
+    // Aquí luego puedes agregar:
+    // - búsqueda dinámica (AJAX)
+    // - filtros
+    // - paginación
+});
